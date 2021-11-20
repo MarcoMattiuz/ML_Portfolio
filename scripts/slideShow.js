abaae5f5a7b50982ce87ede440abaff3fa19c1ea
MarcoMattiuz <marccs35@gmail.com>
2021-11-20 22:40:57 +0100

    slideShow, scrollBar

--- scripts/slideShow.js
+++ scripts/slideShow.js
@@ -2,8 +2,6 @@ const track = document.querySelector(".carousel_track");
 const slides = Array.from(track.children);
 const nextButton = document.querySelector(".carousel_button--right");
 const prevButton = document.querySelector(".carousel_button--left");
-const dotsNav = document.querySelector(".carousel_nav");
-const dots = Array.from(dotsNav.children);
 
 
 const slideWidth = slides[0].getBoundingClientRect().width;
