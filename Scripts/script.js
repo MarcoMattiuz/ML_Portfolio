223307365f4eb716cdbe1691e2c29b34c6d2af61
Lorenzo Boiko <lorenzoboiko@Lorenzos-MacBook-Air.local>
2021-11-22 21:18:30 +0100

    sdf

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,16 +6,22 @@ window.onload=function(){
     crossorigin="anonymous";
     
     const hamburgerButton = document.querySelector('.hamburger-menu');
-    
+    const hamburgerMenu = document.querySelector('.hamburger-menu-container');
+   
     let hamburgerMenuOpen = false;
     if(hamburgerButton){
     hamburgerButton.addEventListener("click", () =>{
 
         if(!hamburgerMenuOpen){
+            hamburgerMenu.classList.add('open');
             hamburgerButton.classList.add('open');
+            document.querySelector('.contact-button').style.display='none';
             hamburgerMenuOpen = true;
         }else{
+            hamburgerMenu.classList.remove('open');
             hamburgerButton.classList.remove('open');
+            document.querySelector('.contact-button').style.display='flex';
+
             hamburgerMenuOpen = false;
         }
     });
