e8445eee9ad70a5f87f741352432901c2f7cd802
MarcoMattiuz <marccs35@gmail.com>
2021-11-25 22:35:23 +0100

    added decorative elements

--- Scripts/background.js
+++ Scripts/background.js
@@ -8,17 +8,38 @@ window.addEventListener("load", () => {
     var about = document.querySelector(".parallax_title");
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
-    
+
     var cont = 0;
     console.log(cont);
-    
-    
+
+
+    var lastScrollTop = 0;
+
+    // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+    // element.addEventListener("scroll", function () { 
+    //     var st = window.pageYOffset || document.about.scrollTop; 
+    //     if (st > lastScrollTop) {
+    //         console.log("down");
+    //         cont++;
+    //         console.log(cont);
+    //         about.style.left = cont + "px";
+    //     } else {
+    //         console.log("down");
+    //         cont++;
+    //         console.log(cont);
+    //         about.style.left = -cont + "px";
+    //     }
+
+    // }, false);
 
     container.addEventListener("scroll", a => {
         cont++;
         console.log(cont);
-        about.style.left = cont+"px";
+        about.style.left = cont*8+"px";
     })
+
+   
+
 
 });
 
