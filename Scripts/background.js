3218d860611c7c7821d7e27c695186d4b4cd4648
MarcoMattiuz <marccs35@gmail.com>
2021-11-26 15:01:32 +0100

    Divided in testBranch

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,36 +1,16 @@
 //prova
 
 window.addEventListener("load", () => {
-    const canvas = document.querySelector("canvas");
-    const ctx = canvas.getContext("2d");
+   
 
     const container = document.querySelector(".container");
     var about = document.querySelector(".parallax_title");
+    var circles = document.querySelector('.decorative_circle');
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
 
     var cont = 0;
     console.log(cont);
-
-
-    var lastScrollTop = 0;
-
-    // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-    // element.addEventListener("scroll", function () { 
-    //     var st = window.pageYOffset || document.about.scrollTop; 
-    //     if (st > lastScrollTop) {
-    //         console.log("down");
-    //         cont++;
-    //         console.log(cont);
-    //         about.style.left = cont + "px";
-    //     } else {
-    //         console.log("down");
-    //         cont++;
-    //         console.log(cont);
-    //         about.style.left = -cont + "px";
-    //     }
-
-    // }, false);
 
     container.addEventListener("scroll", a => {
         cont++;
@@ -44,6 +24,3 @@ window.addEventListener("load", () => {
 });
 
 
-
-// ctx.fillStyle= "red";
-// ctx.fillRect(100, 100, 100, 100);
