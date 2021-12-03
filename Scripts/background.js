e619a340dd10ccb23645c87a92c197530f278ad3
MarcoMattiuz <marccs35@gmail.com>
2021-12-03 19:27:43 +0100

    --

--- Scripts/background.js
+++ Scripts/background.js
@@ -5,14 +5,12 @@ window.addEventListener("load", () => {
 
     const container = document.querySelector(".container");
     var about = document.querySelector(".parallax_title");
-    var circles = document.querySelector('.decorative_circle');
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+   
 
     var cont = 0;
     console.log(cont);
 
-    container.addEventListener("scroll", a => {
+    document.addEventListener("scroll", a => {
         cont++;
         console.log(cont);
         about.style.left = cont*8+"px";
