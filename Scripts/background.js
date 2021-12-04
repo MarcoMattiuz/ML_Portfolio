3d74397c5b7dbf7630848d23cfb660aff7333ff0
MarcoMattiuz <marccs35@gmail.com>
2021-12-04 22:29:54 +0100

    --

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,29 +1,29 @@
 //prova
 
-window.addEventListener("load", () => {
-    var title_about = document.querySelector("#about");
-    var title_skill = document.querySelector("#skill");
+// window.addEventListener("load", () => {
+// var title_about = document.querySelector("#about");
+// var title_skill = document.querySelector("#skill");
 
-    var cont = 0;
-    console.log(cont);
+// var cont = 0;
+// console.log(cont);
 
-    document.addEventListener("scroll", a => {
-        cont++;
-        console.log(cont);
-        title_about.style.left = cont+"px";
-    })
-
-
-    var cont1 = 0;
-    console.log(cont);
-
-    document.addEventListener("scroll", a => {
-        cont1++;
-        console.log(cont1);
-        title_skill.style.left = cont1+"px";
-    })
+// document.addEventListener("scroll", a => {
+//     cont++;
+//     console.log(cont);
+//     title_about.style.left = cont+"px";
+// })
 
 
-   
-});
+// var cont1 = 0;
+// console.log(cont);
+var vh=Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);;
+
+console.log(vh);
+window.addEventListener("scroll", a => {
+    console.log("ciao");
+})
+
+
+
+// });
 
