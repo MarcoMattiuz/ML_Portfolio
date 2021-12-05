40d752bf216cabac4988ae66f3d0807de70890ff
MarcoMattiuz <marccs35@gmail.com>
2021-12-05 15:01:46 +0100

    --

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,27 +1,21 @@
 //prova
 
 // window.addEventListener("load", () => {
-// var title_about = document.querySelector("#about");
+ var title_about = document.querySelector("#title-about");
+ console.log(title_about);
 // var title_skill = document.querySelector("#skill");
 
-// var cont = 0;
-// console.log(cont);
 
-// document.addEventListener("scroll", a => {
-//     cont++;
-//     console.log(cont);
-//     title_about.style.left = cont+"px";
-// })
-
-
-// var cont1 = 0;
-// console.log(cont);
+var cont = 0;
+console.log(cont);
 var vh=Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);;
 console.log(vh);
 
 
 window.addEventListener("scroll", a => {
-    console.log("ciao");
+    cont++;
+    title_about.style.left= cont+"px";
+    console.log(cont);
 })
 
 
