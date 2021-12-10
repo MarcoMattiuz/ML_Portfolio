1e007701b4c83d066f7c9d2eb8fcbc7f545a27d3
MarcoMattiuz <marccs35@gmail.com>
2021-12-10 20:44:08 +0100

    fullPage.js, page 2

---
+++ Scripts/fullPageOptions.js
@@ -0,0 +1,14 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    anchors: ['section1','section2','section3','section4','section5'],
+    navigationTooltips: ['Home','Abouts us','Skills','Projects','Contact us'],
+    showActiveTooltip: true,
+    scrollingSpeed: 550,
+    fitToSection: true,
+	fitToSectionDelay: 500,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    controlArrows: false,
+    slidesNavigation: true
+})
--- Scripts/glideOptions.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    type: "carousel",
-    // autoplay: 4000 | true,
-    hoverpause: true,
-    keyboard: true,
-  };
-  new Glide(".glide", config).mount();
