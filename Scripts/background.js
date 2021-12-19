db557f185185c0f1f43e87737a5c63ee5bd3fd2f
MarcoMattiuz <marccs35@gmail.com>
2021-12-19 17:52:24 +0100

    --

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,12 +1,12 @@
 //prova
 
 // window.addEventListener("load", () => {
- var title_about = document.querySelector("#title-about");
- var title_Skill = document.querySelector("#title-skill");
- var title_projects = document.querySelector("#title-projects");
- var title_contact = document.querySelector("#title-contact");
+var title_about = document.querySelector("#title-about");
+var title_Skill = document.querySelector("#title-skill");
+var title_projects = document.querySelector("#title-projects");
+var title_contact = document.querySelector("#title-contact");
 
- console.log(title_about);
+console.log(title_about);
 
 
 var cont = 0;
@@ -17,14 +17,10 @@ var cont = 0;
 
 window.addEventListener("scroll", a => {
     cont = window.scrollY;
-    title_about.style.left=Math.max( -5 + 0.07  * window.scrollY,-1) + '%';
-    // title_about.style.transform="skewX("+   0.03*window.scrollY   + 'deg'+")";
-    title_Skill.style.left=Math.max(160 - 0.07  * window.scrollY,-10) + '%';
-    // title_Skill.style.transform="skewX("+ -0.03*window.scrollY + 'deg'+")";
-    // title_projects.style.transform="skewX("+0.01*window.scrollY+ 'deg'+")";
-    title_projects.style.left=Math.max(-170 + 0.07  * window.scrollY,-50) + '%';
-    title_contact.style.left=Math.max(+300 - 0.07  * window.scrollY,-50) + '%';
-    // title_contact.style.transform="skewX("+0.015*window.scrollY+ 'deg'+")";
+    title_about.style.left = Math.max(-5 + 0.07 * window.scrollY, -1) + '%';
+    title_Skill.style.left = Math.max(160 - 0.07 * window.scrollY, -10) + '%';
+    title_projects.style.left = Math.max(-170 + 0.07 * window.scrollY, -50) + '%';
+    title_contact.style.left = Math.max(+300 - 0.07 * window.scrollY, -50) + '%';
     console.log(window.scrollY);
 })
 
